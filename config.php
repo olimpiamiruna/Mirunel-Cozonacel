6e6cccf3098114e29ae926ce0b3107cf34c90749
Olimpia Bobu <olimpiamiruna@gmail.com>
2023-11-03 22:42:41 +0200

    home & about

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','bookstore') or die('connection failed');
+$conn = mysqli_connect('localhost','root','','store') or die('connection failed');
 
 ?>
